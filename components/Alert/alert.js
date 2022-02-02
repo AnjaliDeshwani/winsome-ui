767a6b9172e001ce484602d075aeb7a31d07b304
AnjaliDeshwani <deshwanianjali@gmail.com>
2022-02-03 00:50:48 +0530

    feat: added alert and button component

---
+++ components/Alert/alert.js
@@ -0,0 +1,6 @@
+var alertCloseBtn = document.getElementById("alert-close-btn");
+var alertCloseBox = document.getElementById("alert-close-box");
+
+alertCloseBtn.addEventListener("click", () => {
+  alertCloseBox.style.display = "none";
+});
